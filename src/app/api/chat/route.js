f91f818e7fadade9146517d7ca613310f91958d0
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-07-19 17:23:32 +0700

    create api chat route

---
+++ src/app/api/chat/route.js
@@ -0,0 +1,22 @@
+import { createOpenAI } from "@ai-sdk/openai";
+import { streamText, smoothStream } from "ai";
+
+export const maxDuration = 30;
+
+export const POST = async (req) => {
+  const { messages } = await req.json();
+
+  const openai = createOpenAI({
+    baseURL: 'https://models.inference.ai.azure.com',
+    apiKey: process.env.GITHUB_TOKEN,
+  })
+
+  const result = streamText({
+    model: openai("gpt-4.1"),
+    system: "You are a helpful AI assistant named 'San'.",
+    messages,
+    experimental_transform: smoothStream(),
+  })
+
+  return result.toDataStream()
+}
